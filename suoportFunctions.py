d46a030c82fafcad7cef51feed2cb43231bded5b
Shengkai Chen <shengkaikenchen@gmail.com>
2023-01-11 15:10:28 -0500

    Update suoportFunctions.py

--- suoportFunctions.py
+++ suoportFunctions.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, request, flash, redirect, Response, ur
 from difflib import SequenceMatcher
 from nltk.corpus import stopwords
 import nltk
-nltk.download('stopwords')
+nltk.download("stopwords")
 
 
 def read_zip_file(zip_name):
@@ -20,7 +20,6 @@ def read_zip_file(zip_name):
 
     path = str(os.getcwd() + "/" + zip_name + "/*.wav")
     folder = glob.glob(path)
-
     return sorted(folder, reverse=False)
 
 
@@ -30,7 +29,7 @@ def speech_to_text(file):
     # setting up params models
     params = {"model": "en-US_Multimedia", "smart_formatting": "true", "background_audio_suppression": "0.6"}
     headers = {"Content-Type": "audio/wav"}
-    result = requests.post(speech_to_text_url, headers=headers, params=params, data=file)  # data=open(file, 'rb')
+    result = requests.post(speech_to_text_url, headers=headers, params=params, data=file)  # data=open(file, "rb")
 
     # get transcript from json result
     output = ""
@@ -39,7 +38,6 @@ def speech_to_text(file):
     for r in results_data:
         for transcript in r["alternatives"]:
             output = output + " " + transcript["transcript"]
-
     return output
 
 
@@ -49,11 +47,11 @@ def text_to_speech(texts, name):
     os.system(bash_command)
 
     # speech url, here port defines on blsasis of kubernetes port forword
-    text_to_speech_url = 'http://localhost:1080/text-to-speech/api/v1/synthesize'
+    text_to_speech_url = "http://localhost:1080/text-to-speech/api/v1/synthesize"
     # setting up the headers for post request to service
-    headers = {'Content-Type': 'application/json', 'Accept': 'audio/wav'}
-    # etting up params
-    params = {'output': 'output_text.wav'}
+    headers = {"Content-Type": "application/json", "Accept": "audio/wav"}
+    # setting up params
+    params = {"output": "output_text.wav", "rate_percentage": -3, "pitch_percentagequery": 0}
     # creating a data in JSON format to send as a parameter to the service
     words = json.dumps({"text": texts})
     # method to get the Voice data from the text service
@@ -62,13 +60,13 @@ def text_to_speech(texts, name):
     if request.status_code != 200:
         print("TTS Service status:", request.text)
         print("Creating file ---", name)
-    with open(name, mode='bx') as f:
+    with open(name, mode="bx") as f:
         f.write(request.content)
 
 
 def get_keywords(text):
     pizza_size = ["large", "medium", "small"]
-    pizza_topping = ["pepperoni", "mushroom", "onion", "black olive", "green pepper", "cheese"]
+    pizza_topping = ["pepperoni", "bacon", "chicken", "anchovies", "mushroom", "onion", "black olive", "green pepper"]
     order_size, order_topping = [], []
     check_size = True
 
@@ -85,17 +83,15 @@ def get_keywords(text):
 
     # extractor = yake.KeywordExtractor(lan="en", n=3, dedupLim=0.9, top=10, features=None)
     # keywords = extractor.extract_keywords(text)
-
     return order_size, order_topping
 
 
 def clean_text(text):
-    stop_words = stopwords.words('english')
-    stop_words.extend(["gimme", "lemme", "cause", "'cuz", "imma", "gonna", "wanna", "please",
+    stop_words = stopwords.words("english")
+    stop_words.extend(["gimme", "lemme", "cause", "cuz", "imma", "gonna", "wanna", "please",
                        "gotta", "hafta", "woulda", "coulda", "shoulda", "howdy", "day",
                        "hey", "yoo", "deliver", "delivery", "delivered", "piece", "want", "order"])
-    clean_text = " ".join([word.replace('X', '').replace('/', '') for word in text.split() if word.lower() not in stop_words])
-
+    clean_text = " ".join([word.replace("X", "").replace("/", "") for word in text.split() if word.lower() not in stop_words])
     return clean_text
 
 
@@ -105,7 +101,6 @@ def get_local_audio_text(file, file_name):
         print("file uploaded successfully")
     with open(file_name, "rb") as audio:
         text = speech_to_text(audio)
-
     return text
 
 
